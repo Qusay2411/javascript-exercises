ccd37900f6fe16e0aaf1a38597e0349601fb0556
Qusay2411 <aqwcharles211@gmail.com>
2023-10-13 18:29:25 +0100

    Completed exercise 4 and 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,32 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
 
-};
+
+    let newArr = [];
+
+    // args[0].forEach((item) => {
+    //     if (!args.includes(item)) {
+    //         newArr.push(item);
+    //     }
+    // });
+    // // console.log(args[0]);
+    // console.log(newArr);
+
+    for(let i = 0; i < args[0].length;i++) {
+        // console.log(args[0][i]);
+        if (args.includes(args[0][i])) {
+            continue;
+        }
+        else {
+            newArr.push(args[0][i]);
+        }
+    }
+    console.log(newArr);
+    return newArr;
+}
+
+
+
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3, ["hey", 2, 3, "ho"])
 
 // Do not edit below this line
 module.exports = removeFromArray;
