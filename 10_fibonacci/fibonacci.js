0dcc91f7539e772daf83d953e32b5644d1fb23fc
Qusay2411 <aqwcharles211@gmail.com>
2023-10-14 01:44:49 +0100

    Completed Fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+
+    if (n < 0){
+        return "OOPS";
+    }
+    prev = 1
+    next = 1
+    // [1,1,2]
+    for(let i = 1; i < n; i++){
+        let temp = next;
+        next = next + prev;
+        prev = temp;
+        console.log([prev, next])
+        console.log(i);
+    }
+    return prev;
 
 };
 
