41b75aabb570d56bbe72d57a9481faff603c3ea7
Qusay2411 <aqwcharles211@gmail.com>
2023-10-14 01:28:27 +0100

    Completed Palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const stringRemove = str.toLowerCase().replace(/[^0-9a-z]/gi, '');
+    const reverseString = stringRemove.split("").reverse().join("");
+    console.log(reverseString, stringRemove);
+    return stringRemove === reverseString
 };
 
 // Do not edit below this line
